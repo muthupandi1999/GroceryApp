760ff41da2e24eba68b5d31f9cdc0872bfb430f0
Prithiviraj1397 <prithiviraj@colanonline.com>
2023-11-14 12:47:00 +0530

    user mail code optimized

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -6,6 +6,7 @@ import {
   sendPushNotificationToOne,
   sendPushNotificationToMulti,
 } from "./sendPushNotification";
+import transporter from "../services/mail.service";
 
 export const createAddress = async (input: Address) => {
   const { address, apartment, label, pincode } = input;
@@ -126,4 +127,284 @@ export const createStripeCustomer = async (name: string, email: string) => {
     email: email,
   })
   return customer.id
+}
+
+export const sendMail = async (email: string, otp: number) => {
+  const mailConfigurations = {
+    // It should be a string of sender/server email
+    from: "mahespandi0321@gmail.com",
+    to: email,
+    // Subject of Email
+    subject: "Email Verification",
+    // This would be the text of email body
+    html: `<!DOCTYPE html>
+    <html>
+      <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <style type="text/css">
+          /* FONTS */
+          @media screen {
+            @font-face {
+              font-family: 'Lato';
+              font-style: normal;
+              font-weight: 400;
+              src: local('Lato Regular'), local('Lato-Regular'),
+                url(https://fonts.gstatic.com/s/lato/v11/qIIYRU-oROkIk8vfvxw6QvesZW2xOQ-xsNqO47m55DA.woff)
+                  format('woff');
+            }
+    
+            @font-face {
+              font-family: 'Lato';
+              font-style: normal;
+              font-weight: 700;
+              src: local('Lato Bold'), local('Lato-Bold'),
+                url(https://fonts.gstatic.com/s/lato/v11/qdgUG4U09HnJwhYI-uK18wLUuEpTyoUstqEm5AMlJo4.woff)
+                  format('woff');
+            }
+    
+            @font-face {
+              font-family: 'Lato';
+              font-style: italic;
+              font-weight: 400;
+              src: local('Lato Italic'), local('Lato-Italic'),
+                url(https://fonts.gstatic.com/s/lato/v11/RYyZNoeFgb0l7W3Vu1aSWOvvDin1pK8aKteLpeZ5c0A.woff)
+                  format('woff');
+            }
+    
+            @font-face {
+              font-family: 'Lato';
+              font-style: italic;
+              font-weight: 700;
+              src: local('Lato Bold Italic'), local('Lato-BoldItalic'),
+                url(https://fonts.gstatic.com/s/lato/v11/HkF_qI1x_noxlxhrhMQYELO3LdcAZYWl9Si6vvxL-qU.woff)
+                  format('woff');
+            }
+          }
+    
+          /* CLIENT-SPECIFIC STYLES */
+          body,
+          table,
+          td,
+          a {
+            -webkit-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+          }
+          p {
+            margin: 0;
+          }
+    
+          table,
+          td {
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
+          }
+    
+          img {
+            -ms-interpolation-mode: bicubic;
+          }
+    
+          /* RESET STYLES */
+          img {
+            border: 0;
+            height: auto;
+            line-height: 100%;
+            outline: none;
+            text-decoration: none;
+          }
+    
+          table {
+            border-collapse: collapse !important;
+          }
+    
+          body {
+            height: 100% !important;
+            margin: 0 !important;
+            padding: 0 !important;
+            width: 100% !important;
+          }
+    
+          a {
+            color: #a1a1a1;
+          }
+          a:hover {
+            color: #a1a1a1;
+          }
+          a:active {
+            color: #a1a1a1;
+          }
+          a:visited {
+            color: #a1a1a1;
+          }
+    
+          /* iOS BLUE LINKS */
+          a[x-apple-data-detectors] {
+            color: inherit !important;
+            text-decoration: none !important;
+            font-size: inherit !important;
+            font-family: inherit !important;
+            font-weight: inherit !important;
+            line-height: inherit !important;
+          }
+    
+          /* ANDROID CENTER FIX */
+          div[style*='margin: 16px 0'] {
+            margin: 0 !important;
+          }
+        </style>
+      </head>
+    
+      <body
+        style="
+          background-color: #ededed;
+          margin: 0 !important;
+          padding: 20px 0 !important;
+        "
+      >
+        <table
+          style="font-family: 'Lato'"
+          border="0"
+          cellpadding="0"
+          cellspacing="0"
+          width="100%"
+        >
+          <tr>
+            <td bgcolor="#ededed" align="center">
+              <!-- 컨텐츠 영역 -->
+              <table
+                border="0"
+                cellpadding="0"
+                cellspacing="0"
+                width="100%"
+                style="max-width: 600px"
+              >
+                <tr>
+                  <td bgcolor="#f74658" align="center">
+                    <table
+                      border="0"
+                      cellpadding="0"
+                      cellspacing="0"
+                      width="100%"
+                      style="max-width: 540px"
+                    >
+                      <!-- 최상단 ENDAND 로고 -->
+                      <tr>
+                        <td style="padding: 24px 6px">
+                          <a href="https://endand.com" target="_blank">
+                            <img
+                              alt="Logo"
+                              src="https://s3cf.endand.com/assets/logo-white.png"
+                              width="120"
+                              border="0"
+                              style="
+                                display: block;
+                                width: 120px;
+                                max-width: 120px;
+                                min-width: 120px;
+                              "
+                            />
+                          </a>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+                <tr>
+                  <td bgcolor="#f74658">
+                    <table
+                      border="0"
+                      cellpadding="0"
+                      cellspacing="0"
+                      width="100%"
+                      style="max-width: 600px"
+                    >
+                      <tr>
+                        <td align="center">
+                          <img
+                            alt="Logo"
+                            src="https://s3cf.endand.com/assets/verification-email.png"
+                            width="120"
+                            border="0"
+                            style="
+                              display: block;
+                              width: 120px;
+                              max-width: 120px;
+                              min-width: 120px;
+                            "
+                          />
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    bgcolor="#f74658"
+                    align="center"
+                    style="
+                      font-size: 36px;
+                      font-weight: bold;
+                      color: white;
+                      padding: 14px 0 50px;
+                    "
+                  >
+                   Verify your email address
+                  </td>
+                </tr>
+                <tr>
+                  <td bgcolor="#ffffff" align="center" style="padding: 28px 6px">
+                    <table
+                      border="0"
+                      cellpadding="0"
+                      cellspacing="0"
+                      width="100%"
+                      style="max-width: 540px"
+                    >
+                      <tr>
+                        <td
+                          align="center"
+                          style="
+                            color: #262626;
+                            font-size: 20px;
+                            font-weight: bold;
+                            padding-bottom: 8px;
+                          "
+                        >
+                          Just one more step...
+                        </td>
+                      </tr>
+                      <tr align="center" style="color: #262626">
+                        <td style="font-size: 14px">
+                          Click the button below to activate your ENDAND account.
+                        </td>
+                      </tr>
+                      <tr>
+                        <td
+                          align="center"
+                          style="padding: 44px 0 60px; font-weight: bold"
+                        >
+                          <!-- ### 이메일 인증 URL -->
+                          <h5>Your Password verification code</h5>
+                          <h3>${otp}</h3>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+                <tr >
+                
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </body>
+    </html>`,
+  };
+
+  transporter.sendMail(mailConfigurations, (error: any, _info: any) => {
+    if (error) throw Error(error);
+  });
 }
