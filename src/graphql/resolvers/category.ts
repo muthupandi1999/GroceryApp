f56910aab7eb40081c0fad6f1c7f70b648e880df
Prithiviraj1397 <prithiviraj@colanonline.com>
2023-11-21 13:40:01 +0530

    catergory check

--- src/graphql/resolvers/category.ts
+++ src/graphql/resolvers/category.ts
@@ -41,7 +41,7 @@ export default {
               products: {
                 include: {
                   image: true,
-                  variant: { include: { ProductInventory: true } },
+                  variant: true
                 },
               },
             },
@@ -73,7 +73,7 @@ export default {
               products: {
                 include: {
                   image: true,
-                  variant: { include: { ProductInventory: true } },
+                  variant: true
                 },
               },
             },
@@ -93,7 +93,7 @@ export default {
           products
         };
       })
-   
+
       return result;
     }
   },
